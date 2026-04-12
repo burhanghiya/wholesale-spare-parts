7ab56ed3520f74478f3a4eef6432281ea5155e75
burhanghiya <burhanghiya32@gmail.com>
2026-04-12 09:58:28 +0530

    Update const.ts

--- client/src/const.ts
+++ client/src/const.ts
@@ -1,8 +1,3 @@
 export const getLoginUrl = () => {
-  const oauthPortalUrl = "https://wholesale-spare-parts-production.up.railway.app";
-  const appId = "patel-spares-shop";
-  const redirectUri = "https://wholesale-spare-parts-production.up.railway.app/api/oauth/callback";
-  const state = btoa(redirectUri);
-  
-  return `${oauthPortalUrl}/login?appId=${appId}&state=${state}`;
+  return "/login"; 
 };
